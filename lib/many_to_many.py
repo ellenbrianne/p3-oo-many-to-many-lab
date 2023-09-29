5dd0db54fcadff49d63c19332d8e70bdfa50ccdc
Ellen Bennett <bennette831@gmail.com>
2023-09-29 14:38:56 -0600

    create Author, Book, and Contract classes, define methods, start property validation for intermediary Contract class

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,42 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def contracts(self):
+        pass
+
+    def books(self):
+        pass
+
+    def sign_contract(self, book, date, royalties):
+        pass
+
+    def total_royalties(self):
+        pass
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+
+    def contracts_by_date(cls, date):
+        pass
 
 
 class Contract:
-    pass
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+    def get_author(self):
+        return self._author
+
+    def set_author(self, author):
+        if isinstance(author, Author):
+            self._author = author
+        else:
+            raise Exception
+        
+    author = property(get_author, set_author)
