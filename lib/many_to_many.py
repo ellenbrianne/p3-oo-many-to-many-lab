1b7be846fad653e700b69714fcb4b203f7974f0c
Ellen Bennett <bennette831@gmail.com>
2023-10-22 05:38:04 -0600

    most recent attempt

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,9 +1,13 @@
 class Author:
+    all = []
+
     def __init__(self, name):
         self.name = name
+        self.all.append(self)
 
     def contracts(self):
-        pass
+        c_list = [c for c in Contract.all if self.name == c.author]
+        print(c_list)
 
     def books(self):
         pass
@@ -16,27 +20,71 @@ class Author:
 
 
 class Book:
+    all = []
+
     def __init__(self, title):
         self.title = title
+        self.all.append(self)
 
     def contracts_by_date(cls, date):
         pass
 
 
 class Contract:
+    all = []
+
     def __init__(self, author, book, date, royalties):
         self.author = author
         self.book = book
         self.date = date
         self.royalties = royalties
+        self.all.append(self)
 
-    def get_author(self):
+    @property
+    def author(self):
         return self._author
 
-    def set_author(self, author):
+    @author.setter
+    def author(self, author):
         if isinstance(author, Author):
             self._author = author
         else:
             raise Exception
+    
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, book):
+        if isinstance(book, Book):
+            self._book = book
+        else:
+            raise Exception
+
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, date):
+        if isinstance(date, str):
+            self._date = date
+        else:
+            raise Exception
+    
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, royalties):
+        if isinstance(royalties, int):
+            self._royalties = royalties
+        else:
+            raise Exception
         
-    author = property(get_author, set_author)
+author = Author("author")
+book = Book("title")
+contract = Contract(author, book, "01/01/2002", 100)
+author.contracts()
