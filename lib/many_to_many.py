f471d1c55647985a00dcff2dbdb84a85c03febff
Ellen Bennett <bennette831@gmail.com>
2023-10-23 04:57:19 -0600

    work on contracts, books, and contracts_by_date methods

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -6,11 +6,12 @@ class Author:
         self.all.append(self)
 
     def contracts(self):
-        c_list = [c for c in Contract.all if self.name == c.author]
-        print(c_list)
+        c_list = [c for c in Contract.all if c.author.name == self.name]
+        return c_list
 
     def books(self):
-        pass
+        b_list = [b for b in Book.all if self.name in self.contracts()]
+        return b_list
 
     def sign_contract(self, book, date, royalties):
         pass
@@ -25,9 +26,6 @@ class Book:
     def __init__(self, title):
         self.title = title
         self.all.append(self)
-
-    def contracts_by_date(cls, date):
-        pass
 
 
 class Contract:
@@ -83,8 +81,16 @@ class Contract:
             self._royalties = royalties
         else:
             raise Exception
-        
-author = Author("author")
+
+    @classmethod    
+    def contracts_by_date(cls):
+        def helper(c):
+            return c.date
+
+        print(cls.all.sort(key=helper))
+
+    
+author = Author("name")
 book = Book("title")
-contract = Contract(author, book, "01/01/2002", 100)
-author.contracts()
+contract_1 = Contract(author, book, "02/16/2010", 10)
+contract_2 = Contract(author, book, "01/15/2005", 15)
