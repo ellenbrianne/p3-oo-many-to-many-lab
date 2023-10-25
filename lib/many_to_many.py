ecf45c7d7ba9ea90d536b1a2c68e23c4c3c50893
Ellen Bennett <bennette831@gmail.com>
2023-10-24 18:28:32 -0600

    create remaineder of methods according to deliverables, not passing tests for contracts methods in Author and Book or total_royalties method in Author

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -5,20 +5,28 @@ class Author:
         self.name = name
         self.all.append(self)
 
+    # working, test != passing
     def contracts(self):
-        c_list = [c for c in Contract.all if c.author.name == self.name]
+        c_list = [c for c in Contract.all if c.author.name == self.name] 
         return c_list
-
+    
     def books(self):
-        b_list = [b for b in Book.all if self.name in self.contracts()]
-        return b_list
+        matching_c = [c for c in Contract.all if c.author.name == self.name]
+        matching_b = []
+        for b in Book.all:
+            for c in matching_c:
+                if b.title == c.book.title:
+                    matching_b.append(b)
+        return matching_b
 
     def sign_contract(self, book, date, royalties):
-        pass
+        return Contract(self, book, date, royalties)
 
+    # working, test != passing
     def total_royalties(self):
-        pass
-
+        c_list = self.contracts()
+        r_list = [c.royalties for c in c_list]
+        return sum(r_list)
 
 class Book:
     all = []
@@ -27,6 +35,19 @@ class Book:
         self.title = title
         self.all.append(self)
 
+    # working, test != passing
+    def contracts(self):
+        c_list = [c for c in Contract.all if self.title == c.book.title]
+        return c_list
+
+    def authors(self):
+        matching_c = [c for c in Contract.all if c.book.title == self.title]
+        matching_a = []
+        for a in Author.all:
+            for c in matching_c:
+                if a.name == c.author.name:
+                    matching_a.append(a)
+        return matching_a
 
 class Contract:
     all = []
@@ -84,13 +105,25 @@ class Contract:
 
     @classmethod    
     def contracts_by_date(cls):
-        def helper(c):
-            return c.date
+        c_list = Contract.all
 
-        print(cls.all.sort(key=helper))
+        def myFunc(c):
+            return c.date
+        
+        c_list.sort(key=myFunc)
+        return c_list
 
     
-author = Author("name")
-book = Book("title")
-contract_1 = Contract(author, book, "02/16/2010", 10)
-contract_2 = Contract(author, book, "01/15/2005", 15)
+# author = Author("name")
+
+# author_2 = Author("name_2")
+
+# book = Book("title")
+# book_2 = Book("title_2")
+
+# contract_1 = Contract(author, book, "02/16/2010", 10)
+# contract_3 = Contract(author_2, book, "01/01/2005", 5)
+# contract_2 = Contract(author, book_2, "01/15/2005", 15)
+# contract_4 = Contract(author, book_2, "01/15/2005", 15)
+
+# author.total_royalties()
